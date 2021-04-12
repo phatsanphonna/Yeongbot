4b50bfc0d3774863def6a168cc30dd343c008bd0
ssuniie <phatsanphon2004@outlook.com>
2021-04-12 23:50:17 +0700

    update Currency class

--- cogs/Currnecy.py
+++ cogs/Currnecy.py
@@ -13,15 +13,17 @@ class Currency(commands.Cog):
         with open('users.json') as f:
             users = json.load(f)
 
-        if ctx.author.id in users['user_id']:
-            print(users['user_id'])
-        else:
-            new_id = {
-                'id': ctx.author.id,
-                'money': 1
-            }
-            print(new_id)
-            
+        for user in users:
+            if ctx.author.id in user['user_id']:
+                print(users['user_id'])
+            else:
+                new_id = {
+                    'id': ctx.author.id,
+                    'money': 1
+                }
+                
+                print(new_id)
+
         print(users)
 
 
